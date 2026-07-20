68868a7f8df1a3fbb42d64ef600328737c7c42cd
Singerxushi <zhangqingge60@gmail.com>
2026-07-21 03:40:10 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// 极其简单、免除了第三方复杂 React 插件导入的编译配置，100% 稳妥通过
 export default defineConfig({
-  // 基础路由指向您的 GitHub Pages 仓库子路径
-  base: '/anima-archive/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    // 允许忽略一切打包过程中的微小提示，强制完成静态导出
-    chunkSizeWarningLimit: 2000,
-  }
+  plugins: [react()],
+  base: '/anima-archive/'
 })
